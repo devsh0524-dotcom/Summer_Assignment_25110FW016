6d93baaad5e4487cd2829ea34b0c453cb226b6ca
Dev_sharma52 <devsh0524@gmail.com>
2026-06-10 20:28:51 +0530

    Day2 done

---
+++ Day2/ques1.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int num, sum = 0;
+    cout << "Enter a number: ";
+    cin >> num;
+    While (num != 0){
+        sum = sum + (num % 10);
+        num = num /10;
+    }
+ cout << "Sum of digits = " << sum << endl;
+return 0;
+}
---
+++ Day2/ques3.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int num, digit;
+    long long product = 1;
+    cout << "Enter a number: ";
+    cin >> num;
+    while (num != 0){
+        digit = num % 10;
+        product *= digit;
+        num /= 10;
+    }
+    cout << "Product of digit = " << product << endl;
+    return 0;
+    }
